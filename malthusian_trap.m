9772e31ae1fd3620b40f842ee31bf3d1a18cd75d
Aryaman <aryaman.cis@gmail.com>
2019-04-09 21:44:09 -0400

    Made N explicitly dependent on c. Made c explicitly dependent on L.

--- malthusian_trap.m
+++ malthusian_trap.m
@@ -2,16 +2,26 @@
 %Simulating Malthusian growth with upper limit and consumption booms
 %Aryaman Majumdar
 
-%Call population N and log of population Y
+%Call population N
+%V3: Adding consumption per capita c.
+%Consumption C = z*F(L,N) = z*(L^0.6)*(N^0.4)
+%Consumption per capita c = z*F(L,N)/N = z*f(l) = z*(l^0.6)
 
-N_init = 10;    %Initial population
-N_upper = 100000; %Upper limit given the current income per capita. As of now, this is exogenous
-Y_init = log(N_init);
-Y_upper = log(N_upper);
-r0 = 4; %Exponential growth rate
-k = (Y_upper/Y_init)*(1/(Y_upper - Y_init))*r0;  %Constant needed in logarithmic function
+z = 200000; %Total factor productivity
 
-t = 0:0.05:100;  %time
+N_init = 100000;
+N_upper = 100000;
+N(i) = N_init;
+
+L0 = 100; %Don't know units yet
+L_new = L0;
+L(i) = L0;
+
+c(i) = z*(L0/N(i))^0.6;
+
+r0 = 0.09; %Exponential growth rate
+
+t = 0:0.1:200;  %time
 time_of_last_event = 0; %most recent time at which consumption per capita
                         %experienced an increase
 time_since_event = 0;
@@ -28,38 +38,44 @@ for i = 1:n
     time_since_event = t(i) - time_of_last_event;
     
     %The ln() of the population. The slope of this is the exponential growth rate r
-    Y(i) = Y_upper/(1+(Y_upper/Y_init - 1)*exp(-k*time_since_event));
-   
-    Y(i) = normrnd(Y(i), 0.02*Y(i));
-    if(test==1)
-        disp("Y next");
-        disp(Y(i));
-    end
+    N(i) = N_upper/(1+(N_upper/N_init - 1)*exp(-r0*time_since_event));
+    L(i) = L_new;
+    c(i) = z*(L(i)/N(i))^0.6;
+    
+    c(i) = normrnd(c(i), 0.0002*c(i));
+    N(i) = normrnd(N(i), 0.002*N(i));
+
     %Some low chance of per capita increase because of more inputs or better production. 
-    % Results in increase in growth rate r, which we encode in the variable
-    % k
+    % Right now, c increases only because of an increase in L
     X = rand;
-    if(rand<0.001)
+    if(rand<0.0022)
         time_of_last_event = t(i);
-        Y_init = Y(i);
-        Y_upper = Y_upper*1.6
-        k = (Y_upper/Y_init)*(1/(Y_upper - Y_init))*r;
-        disp("Y_initial");
-        disp(Y_init);
-        disp("Y");
-        disp(Y(i));
         
+        L_new = L(i)*1.05;
+
+        
+        c(i) = z*(L_new/N(i))^0.6; %c increases as a result
+        
+        
+        N_init = N(i);
+        N_upper = N_upper*(L_new/L(i)); %This is the thing I'm unsure of
+
         test=1;
     else
         test=0;
     end
     
     
+    figure(1)
+    plot(t(1:i),c(1:i))
+    xlim([0 200])
+    ylim([3000 3500])
+    pause(0.008)
     
-    plot(t(1:i),Y(1:i))
-    xlim([0 50])
-    ylim([0 30])
-    pause(0.0005)
-    
+    figure(2)
+    plot(t(1:i),N(1:i))
+    xlim([0 200])
+    ylim([80000 200000])
+    pause(0.004)
 
 end
